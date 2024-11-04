6fff652d0ecfa1c93aa0f083ae49b955d5f25388
xeyossr <113219171+xeyossr@users.noreply.github.com>
2024-11-04 15:19:44 +0400

    OSD Keyboard Switch

--- Configs/.local/share/bin/keyboardswitch.sh
+++ Configs/.local/share/bin/keyboardswitch.sh
@@ -6,5 +6,4 @@ source $scrDir/globalcontrol.sh
 hyprctl switchxkblayout all next
 
 layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
-notify-send -a "t1" -r 91190 -t 800 -i "~/.config/dunst/icons/keyboard.svg" "${layMain}"
-
+swayosd-client --custom-message=${layMain} --custom-icon="input-keyboard"
