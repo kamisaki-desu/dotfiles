94133d93153a927b6bcb0d71c97b2756c25fd4ef
xeyossr <113219171+xeyossr@users.noreply.github.com>
2024-11-08 14:27:51 +0400

    .

--- Configs/.local/share/bin/keyboardswitch.sh
+++ Configs/.local/share/bin/keyboardswitch.sh
@@ -5,5 +5,7 @@ source $scrDir/globalcontrol.sh
 
 hyprctl switchxkblayout all next
 
+sleep 0.2
+
 layMain=$(hyprctl -j devices | jq '.keyboards' | jq '.[] | select (.main == true)' | awk -F '"' '{if ($2=="active_keymap") print $4}')
 swayosd-client --custom-message=${layMain} --custom-icon="input-keyboard"
